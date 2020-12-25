8809ddcd18461e6e99b1f18776a35a8e54b5f24a
NeverBehave <gayhub@never.pet>
2020-12-25 05:05:31 -0500

    update entry

--- lambda/fission.js
+++ lambda/fission.js
@@ -1,11 +1,7 @@
 const bot = require('./index')
 
-module.exports = async function(context) {
-    const tmp = context.request.body; // get data passed to us
-    bot.handleUpdate(tmp); // make Telegraf process that data
-    return {
-        status: 200,
-        body: "Hello Xixi-haha-bot!"
-    }
+module.exports = async function(ctx) {
+    await bot.handleUpdate(ctx.request.body, ctx.response); // make Telegraf process that data
+    ctx.status(200).end()
 };
   
