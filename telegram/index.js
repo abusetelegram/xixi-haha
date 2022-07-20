49b1863ef30d5728b0c6c884551de28c34e41898
NeverBehave <gayhub@never.pet>
2022-07-19 20:15:23 -0700

    update bot with v2 json

--- telegram/index.js
+++ telegram/index.js
@@ -16,18 +16,46 @@ const limitConfig = {
 }
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
+const entries_num = Object.keys(json)
+const random = (json) => Math.floor(Math.random() * json.length)
+const fart = () => {
+  const key = Math.floor(Math.random() * entries_num)
+  const art = json[key]
+  const word_key = Math.floor(Math.random() * art.text.length)
+  const word = art.text[word_key]
 
-const random = (json) => json[Math.floor(Math.random() * json.length)]
+  return {
+    text: `${word}
+
+::本条来自[${art.title}](http://jhsjk.people.cn/article/${key}), 作者${art.author}, ${art.editor}`,
+    key, 
+    word_key
+  }
+}
 const randomBoolean = () => Math.random() >= 0.6
 const words = ['怎么？我可是要连任114514年的男人！', '好好学！习得者得永生！'];
 
 const handler = (ctx) => {
-  ctx.telegram.sendMessage(ctx.message.chat.id, random(json), {
-    reply_to_message_id: ctx.message.message_id
+  ctx.telegram.sendMessage(ctx.message.chat.id, fart().text, {
+    reply_to_message_id: ctx.message.message_id, 
+    parse_mode: "markdown"
   })
 }
 
-const inline_handler = (type, text) => {
+const fart_inline_handler = () => {
+  const { text, key, word_key } = fart()
+  return {
+    type: 'article',
+    description: text.length <= 30 ? text.substring(0, 30) : text.substring(0, 30) + '...',
+    id: `${key}-${word_key}`,
+    title: "就地演讲",
+    message_text: text
+  }
+}
+
+const random_inline_handler = () => {
+  const text =  random(words)
+  const type = '大放厥词'
   return {
     type: 'article',
     description: text.length <= 30 ? text.substring(0, 30) : text.substring(0, 30) + '...',
@@ -58,8 +86,8 @@ bot.command('yiyan', handler)
 bot.command('yiyan@xixi_haha_bot', handler)
 bot.on('inline_query', (ctx) => {
   ctx.answerInlineQuery([
-    inline_handler('就地演讲', random(json)),
-    inline_handler('大放厥词', random(words))
+    random_inline_handler(),
+    fart_inline_handler()
   ])
 })
 // Easter EGG
